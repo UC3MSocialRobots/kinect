7b40daaaf444d0ad4fb5d9d1af76dd3747bebab7
Arnaud Ramey <arnaud.a.ramey@gmail.com>
2015-05-18 08:09:14 +0000

    more doc

--- scripts/rosdep.sh
+++ scripts/rosdep.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 #External, ROS and system package dependencies
 
-PACKAGES="ros-`rosversion -d`-openni-camera ros-`rosversion -d`-openni-launch ros-`rosversion -d`-orocos-kinematics-dynamics"
-          #ros-indigo-openni-tracker
-          #ros-indigo-kdl
-
+# ROS packages
+PACKAGES="          ros-`rosversion -d`-openni-camera"
+PACKAGES="$PACKAGES ros-`rosversion -d`-openni-launch"
+PACKAGES="$PACKAGES ros-`rosversion -d`-orocos-kinematics-dynamics"
+PACKAGES="$PACKAGES ros-`rosversion -d`-freenect-launch"
 sudo apt-get install $PACKAGES
 
-# OpenNI
+# OpenNI 1.5
 cd /tmp
 wget http://roboticslab.uc3m.es/mediawiki/images/c/ce/OpenNI-Bin-Dev-Linux-x86-v1.5.4.0.zip
 unzip OpenNI-Bin-Dev-Linux-x86-v1.5.4.0.zip
 cd OpenNI-Bin-Dev-Linux-x86-v1.5.4.0
 sudo bash install.sh
 
-# avin
+# Avin (sensor)
 cd /tmp
 wget http://roboticslab.uc3m.es/mediawiki/images/5/54/Avin2-SensorKinect-v0.93-5.1.2.1-0-g15f1975.zip
 unzip Avin2-SensorKinect-v0.93-5.1.2.1-0-g15f1975.zip
@@ -25,7 +26,7 @@ cd ../Redist/Sensor-Bin-Linux-x86-v5.1.2.1
 sudo chmod a+x install.sh
 sudo sh install.sh
 
-# NiTE
+# NiTE 1.5
 cd /tmp
 wget http://roboticslab.uc3m.es/mediawiki/images/4/49/NITE-Bin-Dev-Linux-x86-v1.5.2.21.zip
 unzip NITE-Bin-Dev-Linux-x86-v1.5.2.21.zip
