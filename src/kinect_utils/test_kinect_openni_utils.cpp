b18909042e7ca870be1a2de9687285b4ea3ad188
Arnaud Ramey <arnaud.a.ramey@gmail.com>
2015-08-18 16:29:25 +0000

    Reorganizing includes

--- src/kinect_utils/test_kinect_openni_utils.cpp
+++ src/kinect_utils/test_kinect_openni_utils.cpp
@@ -42,7 +42,7 @@ Some tests for the utils with Kinect images.
 #if 0
 #include <ros_utils/marker_utils.h>
 #include <ros_utils/pt_utils.h>
-#include "compressed_rounded_image_transport/cv_conversion_float_uchar.h"
+#include "vision_utils/cv_conversion_float_uchar.h"
 
 void reproject_image_ros(const std::string & rgb_depth_filename_prefix,
                          const std::string & kinect_serial_number) {
